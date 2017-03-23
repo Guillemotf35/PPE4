7d100fd3e80f810904d084a9b77c110df3b86613
Maxime <maxime.dupont49280@gmail.com>
2017-03-23 16:58:31 +0100

    Mise à jour fichier java

--- src/Garderie/Garderie.java
+++ src/Garderie/Garderie.java
@@ -7,6 +7,7 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 
@@ -15,7 +16,8 @@ public class Garderie {
 	String idRL;
 	Double prixTotal = 0.0;
 	int nbQuartHeure;
-	private ArrayList<Garderie> responsableLegal = new ArrayList();
+	Double prixQuartHeure = 0.0;
+	private ArrayList<Garderie> lesResponsablesLegal = new ArrayList();
 	
 	void loadDriver() throws ClassNotFoundException{
 		Class.forName("com.mysql.jdbc.Driver");
@@ -27,7 +29,7 @@ public class Garderie {
 		return conn;
 		
 	}
-	
+
 	
 	public void listPersons() throws SQLException {
 		Connection conn = null;
@@ -35,8 +37,22 @@ public class Garderie {
 			conn = newConnection();
 			Statement st = conn.createStatement();
 
+            Scanner sc = new Scanner(System.in);
+            System.out.println("saisir le nom du responsable légal : ");
+            String responsableLegalSelect = sc.nextLine();
 			
-			String queryEnfant = "SELECT nom, prenom, heureArriveeMatin , heureDepartSoir FROM enfant, presence where enfant.id = presence.enfant_id";
+			
+			String queryResponsableLegal = "SELECT responsable_legal.id,nom, prenom, QF, prixQuartHeure FROM responsable_legal, tarif where responsable_legal.tarif_id=tarif.id and nom='"+responsableLegalSelect+"' ";
+			ResultSet rsRL = st.executeQuery(queryResponsableLegal);
+			
+			while (rsRL.next()){
+				idRL = rsRL.getString("id");
+				
+				System.out.printf("%-20s %-20s %-20s %-20s %-20s \n", rsRL.getString("responsable_legal.id"), rsRL.getString("nom") ,rsRL.getString("prenom") ,rsRL.getString("QF"), rsRL.getString("prixQuartHeure"));			
+				prixQuartHeure = rsRL.getDouble("prixQuartHeure");
+			
+			}
+			String queryEnfant = "SELECT enfant.nom, enfant.prenom, heureArriveeMatin , heureDepartSoir , responsable_legal.nom FROM enfant, presence, responsable_legal where enfant.id = presence.enfant_id and responsable_legal.id=enfant.responsable_legal_id and responsable_legal.id = '"+idRL+"' ";
 			ResultSet rsE = st.executeQuery(queryEnfant);
 			
 			while (rsE.next()){
@@ -44,29 +60,22 @@ public class Garderie {
 				nbQuartHeure=(9-rsE.getTime("heureArriveeMatin").getHours())*4;
 				nbQuartHeure = nbQuartHeure + (0-rsE.getTime("heureArriveeMatin").getMinutes()/15);
 				System.out.println(nbQuartHeure);
+				prixTotal = prixTotal + prixQuartHeure*nbQuartHeure ;
+			
 			}			
 			
-			
-			String queryResponsableLegal = "SELECT responsable_legal.id,nom, prenom, QF, prixQuartHeure FROM responsable_legal, tarif where responsable_legal.tarif_id=tarif.id";
-			ResultSet rsRL = st.executeQuery(queryResponsableLegal);
-			
-			while (rsRL.next()){
-				System.out.printf("%-20s %-20s %-20s %-20s %-20s \n", rsRL.getString("responsable_legal.id"), rsRL.getString("nom") ,rsRL.getString("prenom") ,rsRL.getString("QF"), rsRL.getString("prixQuartHeure"));
-				
-				idRL = rsRL.getString("id");
-				prixTotal = rsRL.getDouble("prixQuartHeure")*nbQuartHeure ;
+			System.out.println(prixTotal);
 
-			}
 			
-			String queryNbEnfant = "SELECT count(*) as 'nb' FROM enfant, responsable_legal WHERE enfant.responsable_legal_id=responsable_legal.id and responsable_legal.id = '"+idRL+"' ";
-			ResultSet rsNbE = st.executeQuery(queryNbEnfant);
-			
-			while (rsNbE.next()){
-				System.out.println("nombre d'enfant : ");
-				System.out.printf("%-20s \n", rsNbE.getString("nb"));
-				prixTotal = prixTotal*rsNbE.getInt("nb");
-				System.out.println(prixTotal);
-			}
+//			String queryNbEnfant = "SELECT count(*) as 'nb' FROM enfant, presence WHERE enfant.id=presence.enfant_id and responsable_legal_id ='"+idRL+"' ";
+//			ResultSet rsNbE = st.executeQuery(queryNbEnfant);
+//			
+//			while (rsNbE.next()){
+//				System.out.println("nombre d'enfant : ");
+//				System.out.printf("%-20s \n", rsNbE.getString("nb"));
+//				prixTotal = prixTotal*rsNbE.getInt("nb");
+//				System.out.println(prixTotal);
+//			}
 			
 			
 			
@@ -83,7 +92,8 @@ public class Garderie {
 		}
 	}
 	
-	
+
+
 	public static void main(String[] args) {
 		Garderie test = new Garderie();
 		
